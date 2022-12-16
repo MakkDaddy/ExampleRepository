82c0aa84b761f72a1fcbd238514d1b42c032e7f1
“Colin <colin.hlandmann@gmail.com>
2022-12-15 22:47:13 -0500

    Commit 1

---
+++ Example.java
@@ -0,0 +1,16 @@
+package gitTesting;
+
+public class Example {
+	int a = 1;
+	int b = 2;
+
+	public int add() {
+		int c = a + b;
+		return c;
+	}
+	
+	public int subtract() {
+		int c = a - b;
+		return c;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+package gitTesting;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Example tempExample = new Example();
+		int testInt = tempExample.add();
+		
+		System.out.println(
+				"The add method of tempExample returned the following value: "
+				+ testInt);
+	}
+
+}
